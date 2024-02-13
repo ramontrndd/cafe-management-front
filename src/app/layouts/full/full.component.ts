3973a602069a954b34c6318f2703bbb6c9055b07
Ramon Trindade <ramonbraintrindade@gmail.com>
2024-02-12 23:15:32 -0300

    feat: responsive sidebar implement

--- src/app/layouts/full/full.component.ts
+++ src/app/layouts/full/full.component.ts
@@ -6,16 +6,24 @@ import { ChangeDetectorRef, Component, OnDestroy, AfterViewInit } from '@angular
 @Component({
   selector: 'app-full-layout',
   templateUrl: 'full.component.html',
-  styleUrls: ['./full.component.scss']
+  styleUrls: []
 })
 export class FullComponent implements OnDestroy, AfterViewInit {
- 
+  mobileQuery: MediaQueryList;
 
-  
+  private _mobileQueryListener: () => void;
 
-  constructor() {}
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher
+  ) {
+    this.mobileQuery = media.matchMedia('(min-width: 768px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 
   ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
   }
   ngAfterViewInit() { }
 }
